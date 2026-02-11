8e3696143444812f755a5a6e6af4566deff1e5c9
Samantha Werner <samantha.werner@noaa.gov>
2026-02-11 17:59:11 -0500

    rendering profit chapter

--- scripts/Googledrivedocpages_to_Rbookdown_chapter.R
+++ scripts/Googledrivedocpages_to_Rbookdown_chapter.R
@@ -1,49 +1,9 @@
-
-
-
-# Create the 'chapters' folder if it doesn't exist
-if (!dir.exists("chapters")) {
-  dir.create("chapters")
-}
-
-
-#This bypasses the googledrive package and authentication entirely
-doc_id <- "1qDZh8YXgLPjA6kaZxCOk8FwSFrzFJmzp6y2t_UCJQHc"
-download_url <- paste0("https://docs.google.com/document/d/", doc_id, "/export?format=docx")
-
-download.file(download_url, destfile = "chapters/performancemeasures_chapter.docx", mode = "wb")
-
-
-
-
-
-
-
 # 1. Load necessary libraries
 library(googledrive)
 library(rmarkdown)
 
 # 2. Authenticate (This will open a browser window the first time)
 drive_auth()
-
-# 1. Clear the old session
-drive_deauth()
-
-# 2. Authenticate with a very specific, limited scope
-# This scope only allows R to see the files you tell it to
-drive_auth(scopes = "https://www.googleapis.com/auth/drive.file")
-
-
-# Try this specific way to find the file first
-target_file <- drive_get(as_id(gdoc_link))
-
-# Then download using the object we just found
-drive_download(
-  file = target_file,
-  path = "chapters/performancemeasures_chapter.docx",
-  overwrite = TRUE
-)
-
 
 # 3. Identify your Google Doc by URL or ID
 # Example ID: '1u8yV6_...etc'
@@ -64,3 +24,22 @@ pandoc_convert(
   to = "markdown",
   output = "chapters/02-Performance-Measures.Rmd"
 )
+
+
+
+##is this safe to use?
+## used in alaska https://www.youtube.com/watch?v=4Lo3peWvHbE&t=1s
+
+#additional context: 1. The "Scope" of the Authorization
+#When you run drive_auth(), the browser window will ask you to grant the tidyverse/googledrive app permission to "See, edit, create, and delete all of your Google Drive files."
+
+#The App Level: The R package is granted broad permission so that it can perform any command you type (like drive_find or drive_mkdir).
+
+#2. Broad Access vs. Targeted Action
+#Think of the R package like a Master Key.
+
+#The Authorization (drive_auth): You are handing the R session the master key to your NOAA Google Drive.
+
+#The Action (drive_download): The script only uses that key to open the one specific door (the URL/ID) you pointed to.
+
+#[!IMPORTANT] Privacy Note: The R session "sees" what you see. It will not give your collaborators access to your whole drive; it only gives the code running on your machine the ability to interact with your files. If you share your code with a colleague, they would have to run drive_auth() themselves and would only be able to download the file if they also have permission to view that Google Doc.
