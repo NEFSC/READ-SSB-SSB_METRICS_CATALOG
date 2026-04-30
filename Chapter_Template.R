3e1cd082ffc9cd70a17533313a498ce6fe3cfc97
Samantha Werner <samantha.werner@noaa.gov>
2026-04-30 10:27:20 -0400

    chpater template

---
+++ Chapter_Template.R
@@ -0,0 +1,44 @@
+
+# Cost Data Visualization Tool
+
+## Product Overview
+
+**Text description here*
+
+* Unit:
+* Summary Group(s):
+* Frequency:
+* Time Series:
+
+
+
+### Point of Contact
+
+
+
+### Data Outputs/Outlets
+
+## List of Metrics
+
+
+
+## Metric Descriptions
+
+
+## Additional Methods/Decision Rules
+
+  **add each sub-section with sub headers prefaced by ### . Include any binning/decision rules.
+**also if there are dollar values make sure that is added in a section tilted *Dollar Values
+
+## Data Sources and Code
+
+### Data Sources
+**data used to inform the metrics, including internal and external data (e.g., FRED)
+
+### Code
+Metric Creation Code File Location:
+Read Me file (file to run code):
+Website Production Code Location:
+Read Me file (file to run code):
+
+  **The latter are for if thre is a different location for a stand alone website
