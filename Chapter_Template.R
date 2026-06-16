be1385180bf709fd91160c713416c21e16d61383
Samantha Werner <samantha.werner@noaa.gov>
2026-06-16 09:14:39 -0400

    updates to chapter outline

--- Chapter_Template.R
+++ Chapter_Template.R
@@ -1,24 +1,26 @@
 
 ****This is the outline for the ssb metrics catalog product chapters. Follow the instructions below to build your readme. Reach out if you have any questions
-# [PRODUCT NAME HERE]
+# PRODUCT NAME HERE
 
 ## Product Overview
 
 **Text description here*
 
-* Unit:
-* Summary Group(s):
-* Frequency:
-* Time Series:
+* Unit: [Lowest unit of response, individual, vessel, trip etc. ]
+* Summary Group(s): [Groupings of data when summarized on tools or websites. Note, this may not be applicable for intermediate data that does not have an outward facing tool. In that case, write: N/A]
+* Frequency:  [Interval at which data are reported (e.g., Annual, Monthly, quarterly)]
+* Time Series: [How many years of data are in the dataset. Be Specific, if you have monthly data, provide the years followed by "monthly-level" similar can be done for transaction level data or otherwise.]
 
 
 ### Point of Contact
-
+ [Name and email address]
 
 
 ### Data Outputs/Outlets
+[If the a tool, use the website, if no outward facing tool put N/A or state where it is currently used]
 
 ## List of Metrics
+[ List of individual variables summarized in the tool or website]
 
 
 
@@ -30,6 +32,8 @@
   **add each sub-section with sub headers prefaced by ### . Include any binning/decision rules.
 **also if there are dollar values make sure that is added in a section tilted *Dollar Values
 **addidtional sub sections are welcome (prefaced by #### sub header)
+** This section should be used to give a sense of how the data came to be. If using a model, this should have the technical details of the model. Use the metrics catalog site for examples that fit your project.
+
 
 ## Data Sources and Code
 
@@ -42,4 +46,4 @@ Read Me file (file to run code):
 Website Production Code Location:
 Read Me file (file to run code):
 
-  **The latter are for if thre is a different location for a stand alone website
+  [The latter are for if thre is a different location for a stand alone website]
